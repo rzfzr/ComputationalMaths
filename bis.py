c8334a0bd39ccfcb2c44cecebb1486cb94c02c85
Rafael Figueira Goncalves <capitao.rafa@gmail.com>
2019-10-02 13:24:35 -0300

    Done

--- bis.py
+++ bis.py
@@ -1,10 +1,9 @@
 limit = 10
-# Methods:
-# 0 = Default
-# 1 = Falsa posicao
 
 
-def bis(fun, a, b, tol, method):
+def bisection(fun, a, b, tol):
+    print('bisection')
+
     def f(x):
         return eval(fun)
     counter = 0
@@ -13,10 +12,37 @@ def bis(fun, a, b, tol, method):
         while counter < limit:
             counter += 1
 
-            if method == 0:
-                xm = abs((a+b)/2)
-            elif method == 1:
-                xm = (b*f(a)-a*f(b))/(f(a)-f(b))
+            xm = abs((a+b)/2)
+
+            print('run:', counter, 'xm:', xm, 'a:', a,
+                  'f(a):', f(a), 'b:', b, 'f(b):', f(b))
+
+            if abs(f(xm)) <= tol:
+                return
+            if f(xm) < 0 and f(a) < 0:
+                a = xm
+            elif f(xm) < 0 and f(b) < 0:
+                b = xm
+            elif f(xm) > 0 and f(a) > 0:
+                a = xm
+            elif f(xm) > 0 and f(b) > 0:
+                b = xm
+    else:
+        print('nao tem funcao zero')
+
+
+def falsePosition(fun, a, b, tol):
+    print('falsePosition')
+
+    def f(x):
+        return eval(fun)
+    counter = 0
+    if f(a)*f(b) < 0:
+        print('tem funcao zero')
+        while counter < limit:
+            counter += 1
+
+            xm = (b*f(a)-a*f(b))/(f(a)-f(b))
 
             print('run:', counter, 'xm:', xm, 'a:', a,
                   'f(a):', f(a), 'b:', b, 'f(b):', f(b))
@@ -36,6 +62,8 @@ def bis(fun, a, b, tol, method):
 
 
 def fixedPoint(fun, a, b, tol):
+    print('fixedPosition')
+
     def f(x):
         return eval(fun)
     # gx = (fun+'+x')
@@ -61,12 +89,15 @@ def fixedPoint(fun, a, b, tol):
 
 
 def newton(fun, a, b, tol):
+    print('newton')
+
     def f(x):
         return eval(fun)
 
     def fl(x0):
         return eval('(-1/pow(x0,2))+2*x0')
-    x0 = 2
+
+    x0 = a
     # print(fl(x0))
     if abs(f(x0)) >= tol:
         print('funcao zero: ', x0)
@@ -81,7 +112,14 @@ def newton(fun, a, b, tol):
         print('nao tem funcao zero')
 
 
+def callAll(fun, a, b, tol):
+    bisection(fun, a, b, tol)
+    falsePosition(fun, a, b, tol)
+    fixedPoint(fun, a, b, tol)
+    newton(fun, a, b, tol)
+
+
 # bis('pow(x,3)-7.5*pow(x,2)+12*x+3',4.5,6,0.05, method = 0)
-# bis('4754.28*((1-pow((1+x),-48))/x)-50000',0.09,0.10,0.1, method = 1)
-newton('1/x+pow(x,2)-5', 1, 3, 0.001)
+callAll('4754.28*((1-pow((1+x),-48))/x)-50000', 0.09, 0.10, 0.1)
+# callAll('1/x+pow(x,2)-5', 1, 3, 0.001)
 # fixedPoint('pow(x,3)+8*x-7',0,1,0.1)
